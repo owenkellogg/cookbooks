ee50c02e06a370a570773dcb1e5a32bf7e28c7ec
Steven Zeiler <zeiler.steven@gmail.com>
2014-04-01 21:52:23 -0700

    [FEATURE] Add miniupnp, dogecoind compilation.

--- dogecoind/recipes/default.rb
+++ dogecoind/recipes/default.rb
@@ -7,3 +7,35 @@ package "libdb++-dev"
 package "libboost-all-dev"
 package "libqrencode-dev"
 
+bash "install miniupnp" do
+  user "root"
+  cwd "/home/ubuntu"
+  code <<-EOH
+  curl -L http://miniupnp.free.fr/files/download.php?file=miniupnpc-1.8.tar.gz |tar -xz
+  cd miniupnpc-1.8/
+  make
+  make install
+  cd ..
+  EOH
+end
+
+git "/home/ubuntu/dogecoin" do
+  repository "https://github.com/dogecoin/dogecoin"
+  revision "master"
+  action :sync
+end
+
+
+bash "compile dogecoind" do
+  user "root"
+  cwd "/home/ubuntu"
+  code <<-EOH
+  cd dogecoin/src
+  make -f makefile.unix USE_UPNP=1 USE_QRCODE=1
+  strip dogecoind
+  EOH
+end
+
+
+
+
