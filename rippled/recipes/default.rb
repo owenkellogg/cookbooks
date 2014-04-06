9b55d8ee384e5398bd2b68eba109012edad5e5c7
Steven Zeiler <zeiler.steven@gmail.com>
2014-04-05 19:51:43 -0700

    [FEATURE] Explicitly call install action for rippled dependencies.

--- rippled/recipes/default.rb
+++ rippled/recipes/default.rb
@@ -1,18 +1,37 @@
-
 #include_recipe "harden"
-include_recipe "nodejs"
 
-package "pkg-config"
-package "git"
-package "scons"
-package "ctags"
-package "libboost1.48-all-dev"
-package "protobuf-compiler"
-package "libprotobuf-dev"
+execute "apt-get-update" do
+  command "apt-get update"
+end
+
+package "pkg-config" do
+  action :install
+end
+
+package "git" do
+  action :install
+end
+
+package "scons" do
+  action :install
+end
+
+package "libboost1.48-all-dev" do
+  action :install
+end
+
+package "protobuf-compiler" do
+  action :install
+end
+
+package "libprotobuf-dev" do
+  action :install
+end
+
+include_recipe "nodejs"
 
 git "/opt/rippled" do
   repository "https://github.com/ripple/rippled.git"
   revision  "master"
   action :sync
 end
-
