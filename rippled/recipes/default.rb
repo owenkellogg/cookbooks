57dc6b705972510411c5eea9aced036920802297
Steven Zeiler <zeiler.steven@gmail.com>
2014-04-06 20:22:57 +0000

    [FEATURE] Add manual installation of boost 1.55 from source to rippled.

--- rippled/recipes/default.rb
+++ rippled/recipes/default.rb
@@ -1,12 +1,25 @@
+# Ubuntu 13.10
 
-#include_recipe "harden"
-include_recipe "nodejs"
-
+package "build-essential"
 package "pkg-config"
 package "git"
-package "scons"
 package "ctags"
-package "libboost1.48-all-dev"
+package "scons"
+package "libssl-dev"
+
+# http://www.linuxfromscratch.org/blfs/view/svn/general/boost.html
+bash 'install_boost' do
+  cwd '/home/ubuntu'
+  code <<-EOH
+    wget http://downloads.sourceforge.net/boost/boost_1_55_0.tar.bz2
+    tar jxvf boost_1_55_0.tar.bz2
+    cd boost_1_55_0
+    ./bootstrap.sh --prefix=/usr &&
+    ./b2 stage threading=multi link=shared
+    ./b2 install threading=multi link=shared
+    EOH
+end
+
 package "protobuf-compiler"
 package "libprotobuf-dev"
 
